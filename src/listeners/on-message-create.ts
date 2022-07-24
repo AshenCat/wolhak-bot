818eb73147ba3af0e854fa26c875e13e04d2969f
Klifford Agujar <forddagujar95@gmail.com>
2022-07-24 10:24:49 -0400

    removed on message create

--- src/listeners/on-message-create.ts
+++ src/listeners/on-message-create.ts
@@ -4,13 +4,6 @@ export const onMessageCreate = (client: Client) => {
     client.on('messageCreate', async (message) => {
         if (message.author.bot) return;
         const content = message.content.toLowerCase();
-
-        if (content === 'igor') {
-            await message.reply(`is gae`);
-        }
-
-        if (content === 'klifford') {
-            await message.reply(`not phat`);
-        }
+        return;
     });
 };
