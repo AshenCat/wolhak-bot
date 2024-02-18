8ba8ef17ba5c18ef8d07388975d006c901afb1b9
Klifford Agujar <forddagujar95@gmail.com>
2024-02-18 00:00:20 -0500

    daily inspiration

--- src/helper-functions.ts
+++ src/helper-functions.ts
@@ -1,4 +1,11 @@
 import { CreateChatCompletionResponseChoicesInner } from 'openai';
+import axios from 'axios';
+
+const INSPIRE_URL = 'https://inspirobot.me/api?generate=true';
+
+type InspireResponse = {
+    data: string;
+};
 
 export const getChatGPTResponse = (
     choices: CreateChatCompletionResponseChoicesInner[]
@@ -7,3 +14,19 @@ export const getChatGPTResponse = (
     choices.forEach((msg) => (finalMsg += msg.message?.content));
     return finalMsg;
 };
+
+export function getInspirationImageURL(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await axios.get<InspireResponse['data']>(
+                INSPIRE_URL
+            );
+
+            const inspirationImageURL = response.data;
+
+            resolve(inspirationImageURL);
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
